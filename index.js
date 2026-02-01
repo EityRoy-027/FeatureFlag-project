adb5de67e64c8ab5dbcb552b869b2095cb52b11c
eityroy <eityroy027@gmail.com>
2026-02-02 01:29:48 +0600

    featureflag

--- index.js
+++ index.js
@@ -17,6 +17,16 @@ let on=document.querySelector('#on');
 let del=document.querySelector('#del');
 let circlediv=document.querySelector('#circlediv');
 let circle=document.querySelector('#circle');
+let flag=document.querySelector('#new');
+let newflag=document.querySelector('#newflag');
+let maincontainer=document.querySelector('#maincontainer');
+let cancel=document.querySelector('#cancel');
+let flagcreate=document.querySelector('#flagcreate');
+let inflag1=document.querySelector('#inflag1');
+let inflag2=document.querySelector('#inflag2');
+let rolesel=document.querySelector('#rolesel');
+let analy=document.querySelector('#analy');
+let totalspan=document.querySelector('#totalspan');
 
 signs.addEventListener('click',function(){
     admin.style.display='flex';
@@ -35,8 +45,12 @@ adash.addEventListener('click',function(){
     adash.style.alignItems='center';
     adash.style.color='white';
     adash.style.backgroundColor='rgb(26, 59, 208)';
+    ana.style.backgroundColor='rgb(241, 241, 243)';
+    ana.style.color='black';
+
     mid.style.display='none';
     features.style.display='flex';
+    analy.style.display='none';
 
     slidecircle.addEventListener('input',function(){
         per.textContent=slidecircle.value+'%';
@@ -50,7 +64,75 @@ adash.addEventListener('click',function(){
 
     })
 
-   del.addEventListener('click',function(){
-    featuresbox.remove();
-   }) 
+   
+
+   flag.addEventListener('click',function(){
+   newflag.style.display='flex';
+   maincontainer.style.filter='blur(1px)';
+   })
+
+cancel.addEventListener('click',function(){
+      newflag.style.display='none';
+       maincontainer.style.filter='blur(0px)';
+})
+
+flagcreate.addEventListener('click',function(){
+
+
+features.innerHTML+=`<div class="featuresbox" id="featuresbox">
+
+<div class="featureboxtop">
+
+<div class="nam">${inflag1.value}</div>
+<div class="featureallbut">
+
+    <div class="circlediv" id="circlediv">
+        <div class="circle" id="circle"></div>
+    </div>
+
+    <div class="on" id="on">ON</div>
+    <div class="delete" id="del" onclick="deletebox(this)">Delete</div>
+
+</div>
+
+</div>    
+
+<div class="featurelast">
+<div class="nam2">${inflag2.value}</div>
+<div class="perchan">Rollout Perchantage</div>
+
+<div class="sliders">
+
+
+    <input type="range" class="slidecircle" id="slidecircle"  min="0" max="100" value="25">
+   
+
+<div class="per" id="per">0%</div>
+<div class="perp">Role:${rolesel.value}</div>
+</div>
+</div>
+</div>`
+ 
+totalspan.textContent=parseInt(totalspan.textContent)+1;
+})
+
+})
+function deletebox(e){
+    e.parentElement.parentElement.parentElement.remove();
+}
+
+
+ana.addEventListener('click',function(){
+    analy.style.display='flex';
+    features.style.display='none';
+    ana.style.width='100%';
+    ana.style.height='40px';
+    ana.style.border='none';
+    ana.style.display='flex';
+    ana.style.alignItems='center';
+    ana.style.color='white';
+    ana.style.backgroundColor='rgb(26, 59, 208)';
+    adash.style.backgroundColor='rgb(241, 241, 243)';
+     adash.style.color='black';
+    mid.style.display='none';
 })
